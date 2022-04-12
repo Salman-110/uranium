4692eaa964c72fd7a7a36a4c33ddccbd30c86bf0
Salman-110 <102991176+Salman-110@users.noreply.github.com>
2022-04-12 13:46:24 +0530

    eyyy

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,29 +1,25 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
-        required: true
+const bookSchema = new mongoose.Schema( {
+    bookName:  {
+        type:"string",
+        required: true,
+        unique:true,
+
     },
-    emailId: String,
-    gender: {
+    authorName: "string",
+    category : {
         type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+        enum: ["fiction", "self help", "adventure" ,"fantasy"]
+    } ,
+
+    year: Number
+
+
+    
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('kitaab', bookSchema) //kitabs
 
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,8 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBooksData", UserController.createbook  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getBoosksList", UserController.getBooksData)
 
 module.exports = router;
